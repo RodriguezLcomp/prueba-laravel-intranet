75646f495f7f832c533962c6092b9d61b33f53af
Lautaro Rodriguez <compros.adm02@capsantafe.org.ar>
2021-02-18 11:26:04 -0300

    Se completo el esqueleto de la eliminacion del grupo

--- resources/views/eliminarGrupo.blade.php
+++ resources/views/eliminarGrupo.blade.php
@@ -1,19 +1,30 @@
 @extends('layouts.textPlantilla')
 
 @section('contenido')
-    <h1>Baja de una región</h1>
+    <h1>Baja de un grupo</h1>
     <div class="alert bg-light border-danger col-6 shadow-sm p-4 mx-auto">
         <form action="/eliminarRegion" method="post">
             @csrf
-            Región: <span class="lead">{{ $region->regNombre }}</span>
-            <input type="hidden" name="regID"
-                    value="{{ $region->regID }}">
-            <input type="hidden" name="regNombre"
-                    value="{{ $region->regNombre }}">
+            <input type="text" id="nombreGrupo" name="nombreGrupo"
+            value="{{  $grupo->descgrp }}"
+            class="form-control">
+            <br>
+            Privado:
+            <br>
+            <input type="text" id="privado" name="privado"
+            value="{{ $grupo->privado }}"
+            class="form-control">
+            <br>
+            Intimo:
+            <br>
+            <input type="text" id="qintimo" name="qintimo"
+            value="{{  $grupo->qintimo }}"
+            class="form-control">
+            <br>
             <button class="btn btn-danger btn-block my-2">
                 Confirmar baja
             </button>
-            <a href="/adminRegiones" class="btn btn-outline-secondary btn-block my-2">
+            <a href="/adminGrupos" class="btn btn-outline-secondary btn-block my-2">
                 Volver a panel
             </a>
         </form>
